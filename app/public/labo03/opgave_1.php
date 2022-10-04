45882998b4af0515bd40436be95857d3b5130c20
Siebe Van de Voorde <siebe.vandevoorde@student.odisee.be>
2022-10-04 14:10:46 +0200

    labo03 of 1 gemaakt

---
+++ app/public/labo03/opgave_1.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Lab 03, Exercise 1 & 2 — Start File
+ * @author Bramus Van Damme <bramus.vandamme@odisee.be>
+ */
+
+	// vars
+
+		$basePath = __DIR__ . DIRECTORY_SEPARATOR . 'images'; // check the location on disk!
+		$baseUrl = 'images'; // images
+		$images = []; // An array which will hold all our images
+        $linesArr = [];
+
+	// Main code
+
+		// @TODO Open directory and captions file using some SPL classes
+        $dir = new DirectoryIterator(($basePath));
+        foreach ($dir as $file){
+            if (!$file -> isDot() && !$file -> isDir() && $file -> getExtension() == 'jpg'){
+                $images[] = $file -> getFilename();
+            }
+            else if(!$file -> isDot() && !$file -> isDir()){
+                $caption = $file -> getFilename();
+                $lines = new SplFileObject($basePath.'/'.$caption);
+                foreach ($lines as $line_num => $line) {
+                    $linesArr[] = $line;
+                }
+            }
+        }
+        natsort($images);
+        $imageArr = [];
+        foreach ($images as $key=>$image){
+            $imageArr[$image] = $linesArr[$key];
+        }
+
+		// @TODO loop directory
+
+			// If it's a '.jpg', add it onto an array named $images
+			// Use an associative array so you can store the filename, and caption
+
+
+?><!doctype html>
+<html>
+<head>
+	<title>Images</title>
+	<meta charset="utf-8" />
+	<style>
+
+		body {
+			font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", sans-serif;
+			font-weight: 300;
+			font-size: 14px;
+			line-height: 1.2;
+			background: #FCFCFC;
+		}
+
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+
+		li {
+			display:  block;
+			width: 310px;
+			height: 310px;
+			float: left;
+			border: 1px solid #ccc;
+			margin: 20px;
+			position: relative;
+			box-shadow: 0 0 20px #CCC;
+
+		}
+
+		li img {
+			max-width: 100%;
+		}
+
+		li .caption {
+			display: block;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			padding: 5px;
+			color: #000;
+			background: rgba(255,255,255,0.9);
+			border-top: 1px solid #ccc;
+			text-shadow: 1px 1px 1px #fff;
+		}
+
+		li:hover {
+			box-shadow: 0 0 20px #999;
+		}
+
+	</style>
+</head>
+<body>
+	<ul>
+        <?php
+        foreach ($imageArr as $image => $caption) {
+            echo '<li><img src="'.$baseUrl.'/'.$image.'" alt=""><span class="caption">'.$caption.'</span></li>' . PHP_EOL;
+        }
+        ?>
+	</ul>
+</body>
+</html>
